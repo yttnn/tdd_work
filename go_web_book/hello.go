a7bae7665109e9a7aa805459f532a6cd0d077740
yttnn <67893139+yttnn@users.noreply.github.com>
2024-03-05 15:30:48 +0900

    impl spanish

--- go_web_book/hello.go
+++ go_web_book/hello.go
@@ -4,13 +4,18 @@ import "fmt"
 
 const englishHelloPrefix = "Hello "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola " + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
