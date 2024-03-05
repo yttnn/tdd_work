c5855bdfa30d5c89947f8d85f928689f821dc6e1
yttnn <67893139+yttnn@users.noreply.github.com>
2024-03-05 15:32:03 +0900

    refactor spanish

--- go_web_book/hello.go
+++ go_web_book/hello.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello "
+const spanishHelloPrefix = "Hola "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "Spanish" {
-		return "Hola " + name
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
