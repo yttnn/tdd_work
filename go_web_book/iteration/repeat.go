47159f05b25f9a4a094263a0db26d1ce275282bd
yttnn <67893139+yttnn@users.noreply.github.com>
2024-03-05 21:42:58 +0900

    impl

--- go_web_book/iteration/repeat.go
+++ go_web_book/iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(s string) string {
+func Repeat(s string, n int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		repeated += s
 	}
 	return repeated
--- go_web_book/iteration/repeat_test.go
+++ go_web_book/iteration/repeat_test.go
@@ -31,6 +31,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 1)
 	}
 }
